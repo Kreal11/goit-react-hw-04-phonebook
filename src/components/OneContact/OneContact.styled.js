331213df772933818dd607db2331aae580732add
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-28 00:49:56 +0300

    updated styles for one contact

--- src/components/OneContact/OneContact.styled.js
+++ src/components/OneContact/OneContact.styled.js
@@ -2,17 +2,21 @@ import styled from 'styled-components';
 
 export const StyledOneContactLi = styled.li`
   background-color: lightgray;
-  border-radius: 5%;
+  border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+  min-width: 120px;
+  min-height: 90px;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const OneContactDeleteButton = styled.button`
   max-width: 150px;
   padding: 5px 10px;
-  margin: 0 auto;
+  margin: auto auto 0 auto;
   background: linear-gradient(to bottom, #ff5733, #d32f2f);
   color: #fff;
-  border-radius: 5px;
+  border-radius: 10px;
   border: none;
   cursor: pointer;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
